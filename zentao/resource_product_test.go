5728d84bcb186e10da791f3d9f4d2e325a37becb
HaloGithub <lek.malaysia@gmail.com>
2026-05-03 19:39:50 +0800

    test(zentao): acceptance for replace, import, disappears, bad creds

--- zentao/resource_product_test.go
+++ zentao/resource_product_test.go
@@ -1,14 +1,20 @@
 package zentao
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"testing"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	zentaoapi "github.com/myklst/terraform-provider-st-zentao/zentaoAPI"
 )
 
 var protoV6Factories = map[string]func() (tfprotov6.ProviderServer, error){
@@ -68,4 +74,103 @@ resource "st-zentao_product" "p" {
 			},
 		},
 	})
+}
+
+func TestAccProductResource_requiresReplace(t *testing.T) {
+	code1 := uniqueCode("acc1")
+	code2 := uniqueCode("acc2")
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: protoV6Factories,
+		Steps: []resource.TestStep{
+			{
+				Config: providerBlock() + fmt.Sprintf(`resource "st-zentao_product" "p" { name = "X" code = %q }`, code1),
+			},
+			{
+				Config: providerBlock() + fmt.Sprintf(`resource "st-zentao_product" "p" { name = "X" code = %q }`, code2),
+				Check:  resource.TestCheckResourceAttr("st-zentao_product.p", "code", code2),
+			},
+		},
+	})
+}
+
+func TestAccProductResource_import(t *testing.T) {
+	code := uniqueCode("imp")
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: protoV6Factories,
+		Steps: []resource.TestStep{
+			{
+				Config: providerBlock() + fmt.Sprintf(`resource "st-zentao_product" "p" { name = "Import" code = %q }`, code),
+			},
+			{
+				ResourceName:      "st-zentao_product.p",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccProductResource_disappears(t *testing.T) {
+	code := uniqueCode("dis")
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: protoV6Factories,
+		Steps: []resource.TestStep{
+			{
+				Config: providerBlock() + fmt.Sprintf(`resource "st-zentao_product" "p" { name = "D" code = %q }`, code),
+				Check: resource.ComposeTestCheckFunc(
+					func(s *terraform.State) error {
+						rs, ok := s.RootModule().Resources["st-zentao_product.p"]
+						if !ok {
+							return errors.New("resource missing from state")
+						}
+						id, err := strconv.Atoi(rs.Primary.ID)
+						if err != nil {
+							return err
+						}
+						c, err := zentaoapi.NewClient(os.Getenv("ZENTAO_URL"), os.Getenv("ZENTAO_ACCOUNT"), os.Getenv("ZENTAO_PASSWORD"))
+						if err != nil {
+							return err
+						}
+						return c.DeleteProduct(context.Background(), id)
+					},
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+func TestAccProvider_InvalidCredentials(t *testing.T) {
+	if os.Getenv("ZENTAO_URL") == "" {
+		t.Skip("ZENTAO_URL must be set")
+	}
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: protoV6Factories,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+provider "st-zentao" {
+  url      = %q
+  account  = "definitely-not-a-real-user"
+  password = "wrong"
+}
+
+resource "st-zentao_product" "p" {
+  name = "x"
+  code = %q
+}
+`, os.Getenv("ZENTAO_URL"), uniqueCode("badcred")),
+				ExpectError: regexpInvalidCredentials,
+			},
+		},
+	})
+}
+
+var regexpInvalidCredentials = mustCompile("invalid credentials|wrong account")
+
+func mustCompile(pattern string) *regexp.Regexp {
+	return regexp.MustCompile(pattern)
 }
